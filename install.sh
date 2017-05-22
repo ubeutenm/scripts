e95ff29b72bfff9a6816bf1e8002125bbf8ca42d
Ulrich J. Beutenmüller <Ulrich J. Beutenmller>
2017-05-22 18:06:18 +0200

    initial

---
+++ install.sh
@@ -0,0 +1,5 @@
+apt-get update --fix-missing
+apt-get install -y default-jre
+apt-get install -y unzip
+wget https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle/2.2.5/gatling-charts-highcharts-bundle-2.2.5-bundle.zip
+unzip gatling-charts-highcharts-bundle-2.2.5-bundle.zip
